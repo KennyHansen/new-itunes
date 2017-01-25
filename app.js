7d8951b98e6009f1ea1767de3b825ac803e3c337
KennyHansen <hansen.kenny101@gmail.com>
2017-01-25 14:37:37 -0700

    fixed multiple songs playing at the same time

--- app.js
+++ app.js
@@ -34,9 +34,22 @@ function ItunesController(){
     songDisplay.innerHTML += `</div>`
   }
   
-  function previewMusic(songName) {
-      
+  // event listeners :O
+  function onlyPlayOneIn(container) {
+    container.addEventListener("play", function(event) {
+    audio_elements = container.getElementsByTagName("audio")
+      for(i=0; i < audio_elements.length; i++) {
+        audio_element = audio_elements[i];
+        if (audio_element !== event.target) {
+          audio_element.pause();
+        }
+      }
+    }, true);
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    onlyPlayOneIn(document.body);
+  });
 }
 
 
